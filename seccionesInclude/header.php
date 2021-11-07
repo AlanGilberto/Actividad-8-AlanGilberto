c6f59a44c4b9a83bbc5314c84777733c4537ab34
AlanGilberto <92388642+AlanGilberto@users.noreply.github.com>
2021-11-06 19:50:47 -0600

    Add files via upload

---
+++ seccionesInclude/header.php
@@ -0,0 +1,9 @@
+<header>
+    <h1>CENTRAL CAMIONERA</h1>
+
+    <div class="enlaces">
+        <a href="index.php">Inicio</a>
+        <a href="Chofer.php">Chofer</a>
+        <a href="Destinos.php">Destinos</a>
+    </div>
+</header>
